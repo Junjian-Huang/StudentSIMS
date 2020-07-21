f38495d0ab2f11e6453ace1bdfb4f1c621d0d0a1
Junjian Huang <junjian.huang@hotmail.com>
2020-07-21 21:43:45 +1200

    Add project files.

---
+++ StudentSIMS/Migrations/20200720130206_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentSIMS.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    addressId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    sid = table.Column<int>(maxLength: 100, nullable: false),
+                    streetNumber = table.Column<int>(nullable: false),
+                    street = table.Column<string>(nullable: true),
+                    suburb = table.Column<string>(nullable: true),
+                    city = table.Column<string>(nullable: true),
+                    country = table.Column<string>(nullable: true),
+                    postCode = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.addressId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
---
+++ StudentSIMS/Models/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSIMS.Models
+{
+    public class Address
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int addressId { get; set; }
+        [Required, MaxLength(100)]
+        public int sid { get; set; }
+        public int streetNumber { get; set; }
+        public string street { get; set; }
+        public string suburb { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public int postCode { get; set; }
+
+    }
+}
